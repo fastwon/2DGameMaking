24046fa6aef186d0cfb46823fe25c4cd77e5e527
fastwon <cbkh7@naver.com>
2024-04-06 01:11:50 +0900

    make NPC

--- My2DGame/src/entity/Player.java
+++ My2DGame/src/entity/Player.java
@@ -13,7 +13,6 @@ import main.UtilityTool;
 
 public class Player extends Entity {
 	
-	GamePanel gp;
 	KeyHandler keyH;
 	
 	int accel;
@@ -24,11 +23,10 @@ public class Player extends Entity {
 	public final int screenX;
 	public final int screenY;
 	
-//	public int hasKey = 0;
-	
 	public Player(GamePanel gp, KeyHandler keyH) {
 		
-		this.gp = gp;
+		super(gp);
+		
 		this.keyH = keyH;
 		
 		screenX = gp.screenWidth/2 - (gp.tileSize/2);
@@ -58,31 +56,14 @@ public class Player extends Entity {
 	}
 	public void getPlayerImage() {
 		
-		up1 = setup("boy_up_1");
-		up2 = setup("boy_up_2");
-		down1 = setup("boy_down_1");
-		down2 = setup("boy_down_2");
-		left1 = setup("boy_left_1");
-		left2 = setup("boy_left_2");
-		right1 = setup("boy_right_1");
-		right2 = setup("boy_right_2");
-	}
-	
-	public BufferedImage setup(String imageName) {
-		
-		UtilityTool uTool = new UtilityTool();
-		BufferedImage image = null;
-		
-		try {
-			image = ImageIO.read(getClass().getResourceAsStream("/player/" + imageName + ".png"));
-			image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
-			
-			
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-		
-		return image;
+		up1 = setup("/player/boy_up_1");
+		up2 = setup("/player/boy_up_2");
+		down1 = setup("/player/boy_down_1");
+		down2 = setup("/player/boy_down_2");
+		left1 = setup("/player/boy_left_1");
+		left2 = setup("/player/boy_left_2");
+		right1 = setup("/player/boy_right_1");
+		right2 = setup("/player/boy_right_2");
 	}
 	
 	
@@ -111,6 +92,10 @@ public class Player extends Entity {
 			// CHECK OBJECT COLLISION
 			int objIndex = gp.cChecker.checkObject(this, true);
 			pickUpobject(objIndex);
+			
+			// CHECK NPC COLLSION
+			int npcIndex = gp.cChecker.checkEntity(this, gp.npc);
+			interactNPC(npcIndex);
 			
 			// IF COLLISION IS FALSE, PLAYER CAN MOVE
 			if(collisionOn == false) {
@@ -173,6 +158,12 @@ public class Player extends Entity {
 		
 		if(i != 999) {
 			
+		}
+	}
+	public void interactNPC(int i) {
+		
+		if(i != 999) {
+			System.out.println("you are hitting an npc!");
 		}
 	}
 	
