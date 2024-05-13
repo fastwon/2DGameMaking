8e486087dc2b0aad5a4caadbb21df7811c5ddb2d
fastwon <cbkh7@naver.com>
2024-05-13 22:17:39 +0900

    integrate object into entity and update entity draing logic

--- My2DGame/src/main/UI.java
+++ My2DGame/src/main/UI.java
@@ -9,11 +9,9 @@ import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.DecimalFormat;
 
+import entity.Entity;
 import object.OBJ_Heart;
-import object.OBJ_Key;
-import object.SuperObject;
 
 public class UI {
 
@@ -46,7 +44,7 @@ public class UI {
 		}
 		
 		// CREATE HUD OBJECT
-		SuperObject heart = new OBJ_Heart(gp);
+		Entity heart = new OBJ_Heart(gp);
 		heart_full = heart.image;
 		heart_half = heart.image2;
 		heart_blank = heart.image3;
