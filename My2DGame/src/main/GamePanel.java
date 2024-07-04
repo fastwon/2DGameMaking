a653c65bb29cecc6ccc539e333ab1ee1ce67d88c
fastwon <cbkh7@naver.com>
2024-07-04 21:59:27 +0900

    debug key and map refresh key

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
@@ -182,7 +183,7 @@ public class GamePanel extends JPanel implements Runnable{
 		
 		// DEBUG
 		long drawStart = 0;
-		if(keyH.checkDrawTime) {
+		if(keyH.showDebugText) {
 			drawStart = System.nanoTime();
 		}
 		
@@ -234,12 +235,27 @@ public class GamePanel extends JPanel implements Runnable{
 		
 		
 		//DEBUG
-		if(keyH.checkDrawTime) {
+		if(keyH.showDebugText) {
 			long drawEnd = System.nanoTime();
 			long passed = drawEnd - drawStart;
+			
+			g2.setFont(new Font("Arial", Font.PLAIN, 20));
 			g2.setColor(Color.white);
-			g2.drawString("Draw Time: " + passed, 10, 400);
-			System.out.println("Draw Time: "+passed);
+			int x = 10;
+			int y = 400;
+			int lineHeight = 20;
+			
+			g2.drawString("WorldX" + player.worldX, x, y);	
+			y += lineHeight;
+			g2.drawString("WorldY" + player.worldY, x, y);	
+			y += lineHeight;
+			g2.drawString("Col" + (player.worldX + player.solidArea.x) /tileSize, x, y);	
+			y += lineHeight;
+			g2.drawString("Row" + (player.worldY + player.solidArea.y) /tileSize, x, y);	
+			y += lineHeight;
+			
+			g2.drawString("Draw Time: " + passed, x, y);
+//			System.out.println("Draw Time: "+passed);
 		}
 		
 		g2.dispose();
--- My2DGame/src/main/KeyHandler.java
+++ My2DGame/src/main/KeyHandler.java
@@ -9,7 +9,7 @@ public class KeyHandler implements KeyListener {
 	public boolean upPressed, downPressed, leftPressed, rightPressed, enterPressed;
 	
 	// DEBUG
-	boolean checkDrawTime = false;
+	boolean showDebugText = false;
 	
 	public KeyHandler(GamePanel gp) {
 		this.gp = gp;
@@ -141,12 +141,15 @@ public class KeyHandler implements KeyListener {
 	
 		// DEBUG
 		if(code == KeyEvent.VK_T) {
-			if(checkDrawTime == false) {
-				checkDrawTime = true;
+			if(showDebugText == false) {
+				showDebugText = true;
 			}
-			else if(checkDrawTime == true) {
-				checkDrawTime = false;
+			else if(showDebugText == true) {
+				showDebugText = false;
 			}
+		}
+		if(code == KeyEvent.VK_R) {
+			gp.tileM.loadMap("/maps/worldV2.txt");
 		}
 	}
 	public void pauseState(int code) {
