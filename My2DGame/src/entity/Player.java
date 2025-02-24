acd9b2b4c11b8694914d7d815a62bfaf727d7882
fastwon <cbkh7@naver.com>
2025-02-24 22:28:55 +0900

    new map and new tile setting (with map editor)

--- My2DGame/src/entity/Player.java
+++ My2DGame/src/entity/Player.java
@@ -57,8 +57,10 @@ public class Player extends Entity {
 	}
 	public void setDefaultValues() {
 		
-		worldX = gp.tileSize * 23;
-		worldY = gp.tileSize * 21;
+		worldX = gp.tileSize * 62;
+		worldY = gp.tileSize * 39;
+//		worldX = gp.tileSize * 23;
+//		worldY = gp.tileSize * 21;
 //		worldX = gp.tileSize * 12;
 //		worldY = gp.tileSize * 12;
 		
